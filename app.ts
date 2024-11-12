1b5563ce974d8054f7a5aa5b927a50cf0309f542
Norman Chizzali <nc222gs@student.lnu.se>
2024-11-12 12:49:07 +0100

    set up ts

--- app.ts
+++ app.ts
@@ -1,2 +1,3 @@
-import express from 'express'
+import { app } from './src/express.ts'
 
+app()
---
+++ src/express.ts
@@ -0,0 +1,6 @@
+import express from 'express'
+import session from 'express-session'
+
+
+
+export const app = express()
